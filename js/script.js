95ba9e882f57fd52929d920dc9a36b1e7dc08b73
Yama Roni <yamaroni8@gmail.com>
2024-11-06 20:08:38 +0700

    update javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // Ini Javascripts
+
+console.log('Hello World');
+
+function validateForm() {
+    let beratBadan = document.getElementById('berat-badan').value;
+    // Kondisi Validasi jika beratBadan kosong
+    if (beratBadan == '' || parseInt(beratBadan) < 0) {
+        alert('Inputan Anda Kosong')
+    } else {
+        document.getElementById('result').innerHTML = beratBadan;
+    }
+    console.log(beratBadan);
+}
+
+console.log(document.getElementById('result').innerHTML);
